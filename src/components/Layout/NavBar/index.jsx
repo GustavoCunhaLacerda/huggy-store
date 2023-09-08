adf3b90c8e3dc35cd7953883bfb5412ae9b65e58
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2023-09-07 22:09:45 -0300

    fix: ajustes visuais

--- src/components/Layout/NavBar/index.jsx
+++ src/components/Layout/NavBar/index.jsx
@@ -2,20 +2,25 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   const navOptions = [
-    { label: "Home", path: "/" },
+    // { label: "Home", path: "/" },
     { label: "Ursos", path: "/products" },
-    { label: "Pequenos", path: "/products/category/small" },
-    { label: "Médios", path: "/products/category/medium" },
-    { label: "Grandes", path: "/products/category/large" },
+    // { label: "Pequenos", path: "/products/category/small" },
+    // { label: "Médios", path: "/products/category/medium" },
+    // { label: "Grandes", path: "/products/category/large" },
     { label: "Carrinho", path: "/products/large", icon: "cart" },
     { label: "Perfil", path: "/profile", icon: "person" },
   ];
 
   return (
-    <aside className="flex w-full bg-gray-50 border-b border-gray-300 p-6 justify-between">
+    <aside className="flex w-full p-6 justify-between bg-gray-50 border-b">
       <Link to="/" className="flex gap-2 items-center">
-        <img src="/public/icons/teddy-bear.svg" className="w-8 h-8" />
-        <span className="font-inter font-bold">HUGGY</span>
+        <img src="/public/icons/teddy-bear.svg" className="w-10 h-10" />
+        <span
+          className="font-inter font-normal text-2xl"
+          style={{ color: "#546de5" }}
+        >
+          HUGGY
+        </span>
       </Link>
       <nav className="flex gap-4 items-center">
         {navOptions.map((option, index) => {
@@ -24,7 +29,12 @@ function NavBar() {
               {option.icon ? (
                 <img src={`/icons/${option.icon}.svg`} height={32} width={32} />
               ) : (
-                option.label
+                <span
+                  style={{ color: "#e66767" }}
+                  className="font-semibold text-base"
+                >
+                  {option.label}
+                </span>
               )}
             </Link>
           );
--- src/components/Pages/Products/ProductsItem.jsx
+++ src/components/Pages/Products/ProductsItem.jsx
@@ -8,20 +8,30 @@ function ProductItem({ product }) {
     navigate(path);
   }
 
+  function createTeddyId(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   return (
     <div className="flex flex-col gap-2">
-      <div
-        className="w-[210px] h-[300px] bg-gray-200 cursor-pointer"
-        onClick={() => handleNavigation("/products/" + product.id)}
+      <img
+        src={`/teddies/teddy-${createTeddyId(1, 4)}.png`}
+        className="w-[300px] h-[300px] bg-gray-200 cursor-pointer"
       />
-      <div className="flex flex-col">
-        <span className="text-gray-950 text-xl font-semibold hover:underline cursor-pointer font-inter">
+      <div className="flex flex-col items-center justify-center">
+        <span
+          className="text-gray-950 text-xl font-semibold hover:underline cursor-pointer font-inter"
+          onClick={() => handleNavigation("/products/" + product.id)}
+        >
           {product.name}
         </span>
-        <span className="text-blue-500 text-xl hover:underline cursor-pointer font-inter">
+        <span className="text-xl font-inter" style={{ color: "#e66767" }}>
           R$ {product.price}
         </span>
-        <span>{product.size}</span>
+        <span className="text text-sm " style={{ color: "#596275" }}>
+          10x de R$ {String(Math.floor((product.price / 10) * 100) / 100)} s/
+          juros
+        </span>
       </div>
     </div>
   );
