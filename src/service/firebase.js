0e7f750ba41606992d4c2f6f2e508618c6b1aef0
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2023-10-03 00:32:30 -0300

    add: .env

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,24 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCFezVV4zGCFkPgCNtkDbSg71vRD6yxADQ",
-  authDomain: "huggy-store.firebaseapp.com",
-  projectId: "huggy-store",
-  storageBucket: "huggy-store.appspot.com",
-  messagingSenderId: "5612512489",
-  appId: "1:5612512489:web:72d3cfe0c8dd7ade4f3c78",
-  measurementId: "G-BJVK5NS16M",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-
-// rules_version = '2';
-// service firebase.storage {
-//   match /b/{bucket}/o {
-//     match /{allPaths=**} {
-//      allow read: if true
-//     }
-//   }
-// }
